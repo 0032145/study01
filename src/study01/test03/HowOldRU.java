bcdd0890e4288c632c6af7986b35fcb4cb88618a
Administrator <Administrator@koitt03a-PC>
2019-09-27 15:19:42 +0900

    커밋푸시

--- src/study01/test03/HowOldRU.java
+++ src/study01/test03/HowOldRU.java
@@ -9,6 +9,16 @@ public class HowOldRU {
 		System.out.println("너 몇살이야?");
 		String age = scan.nextLine();
 		System.out.println("난 " + age + "살 이야~");
-		
+		int num = Integer.parseInt(age);
+		if(num<=10) {
+			System.out.println("어린이");
+		}else if(num<=20) {
+			System.out.println("청소년");
+		}else if(num<=40) {
+			System.out.println("청년");
+		}else {
+			System.out.println("ㄲㄷ");
+		}
 	}
 }
+

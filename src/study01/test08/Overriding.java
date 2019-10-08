8003fef1a370e4fc51e6939a2b1efc086bcf66c4
Administrator <Administrator@koitt03a-PC>
2019-10-08 17:45:58 +0900

    2019-10-08

--- src/study01/test08/Overriding.java
+++ src/study01/test08/Overriding.java
@@ -2,17 +2,21 @@ package study01.test08;
 
 class Mother{
 	public void a() {
-		
+		System.out.println("엄마");
 	}
 }
 
 public class Overriding extends Mother {
 
+	
 	public void a() {
-		
+		System.out.println("아들");
+	}
+	public static void main(String[] args) {
+		Overriding or = new Overriding();
+		or.a();
 	}
 }
 /*
- * 상속받아야함
- * 같은 영역 안에서 같은 파라메터를 가지면 오버라이딩
+ * 상속받으면서 같은 파라메터를 가지면 오버라이딩
  */

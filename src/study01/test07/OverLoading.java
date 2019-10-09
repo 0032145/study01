da4990010a85fa5435d5f90b1ffcb37bd4dada0a
Administrator <Administrator@118.37.247.247>
2019-10-09 13:20:45 +0900

    수정

--- src/study01/test07/OverLoading.java
+++ src/study01/test07/OverLoading.java
@@ -4,22 +4,22 @@ public class OverLoading {
 	public void test() {
 		System.out.println("test() 메소드 호출");
 	}
-	public void test(int num) {
-		System.out.println("test(int num)메소드 호출");
+	public void test(int i) {
+		System.out.println("test(int i)메소드 호출");
 	}
 	public void test(String str) {
 		System.out.println("test(String str)메소드 호출");
 	}
-	public void test(String str,int num) {
-		System.out.println("test(String str,int num) 메소드 호출");
+	public void test(String str,int i) {
+		System.out.println("test(String str,int i) 메소드 호출");
 	}
-	public void test(int num, String str) {
-		System.out.println("test(int num,String str) 메소드 호출");
+	public void test(int i, String str) {
+		System.out.println("test(int i,String str) 메소드 호출");
 	}
 	
 	public static void main(String[] args) {
 		OverLoading ol = new OverLoading();
-		ol.test("d",0);
+		ol.test("sdas",23);
 	}
 	/*
 	 * OverLoading 오버로딩
